a87f94b8af6bec559856b3d6ec90b39635dba959
Mykhailo R <rubynskyimykhailo@gmail.com>
2024-03-23 12:09:44 +0100

    Add existing file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 'use client'
 import Auth from '@/app/components/auth'
-/* import { Provider } from 'react-redux'
-import {store} from '@/app/redux/store'
- */
 export default function Home() {
   return (
-    /* {<Provider store={store}> }*/
-      <div className='flex items-center justify-center mt-20'>
-        <Auth/>
-      </div>
-    /* </Provider> */
+    <div className='flex items-center justify-center mt-20'>
+      <Auth/>
+    </div>
   );
 }
